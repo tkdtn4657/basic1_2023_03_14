977c0f2bbe123af2491d3b0084081f0b6a2a0f3a
tkdtn4657 <tkdtn101226@gmail.com>
2023-03-14 10:15:44 +0900

    세팅

---
+++ src/test/java/com/ll/basic1/Basic1ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.ll.basic1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Basic1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
